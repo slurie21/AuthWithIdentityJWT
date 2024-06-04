8e5f30c17963c5eee154a913d1c5bd9b153851fc
Sam Lurie <sjl@lurie.org>
2024-06-04 11:34:29 -0400

    mapped the identity API enpoints using the app.MapIdentityAPI and added enterprise logging model

--- IdentityJWT/Controllers/AuthController.cs
+++ IdentityJWT/Controllers/AuthController.cs
@@ -1,9 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IdentityJWT.Filters.ActionFilters;
+using IdentityJWT.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.ValueGeneration;
 
 namespace IdentityJWT.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
     public class AuthController : ControllerBase
     {
-        
+
+        [HttpPost("register")]
+        [AllowAnonymous]
+        [GetGuidForLogging] 
+        public async Task<IActionResult> Register([FromBody] RegistrationVM registrationVM)
+        {
+            string correlationID = HttpContext.Items["correlationID"].ToString() ?? "";
+            return Created(string.Empty, registrationVM);
+        }
+
     }
 }
